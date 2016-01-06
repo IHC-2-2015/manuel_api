621852bef061e0a43406afd93da7a3abdb67c06b
Ian Mejias <ian.mejias@usach.cl>
2016-01-06 05:25:36 -0300

    CursoAlumno: Se crea la funcion para crear funcionalidades
    Esta funcion puebla la tabla FuncionalidadAyudanteCurso con todas las
    funcionalidades con valor false por defecto.

--- app/models/curso_alumno.rb
+++ app/models/curso_alumno.rb
@@ -2,4 +2,26 @@ class CursoAlumno < ActiveRecord::Base
   belongs_to :grupo
   belongs_to :curso
   belongs_to :alumno, class_name: 'Alumno'
+  has_many :funcionalidades, class_name: 'FuncionalidadAyudanteCurso'
+
+  def crear_funcionalidades
+    begin
+      CursoAlumno.transaction do 
+        func = Funcionalidad.all
+
+        func.each do |f|
+          self.funcionalidades.create(
+            permitido: false,
+            funcionalidad_id: f.id
+          )
+        end 
+      end
+    rescue => e
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  def eliminar_funcionalidades
+    self.funcionalidades.destroy_all
+  end
 end
