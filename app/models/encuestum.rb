5231ba37546b56ff72093f3c605e26e3f0cfccff
Ian Mejias <ian.mejias@usach.cl>
2016-01-17 03:11:45 -0300

    Se muestran las preguntas dentro del mismo .json de la encuesta
    Las preguntas se colocan dentro de .json de la encuesta como un arreglo.
    Es decir el .json de una encuesta en particular se verá algo como:
    
    {
     "id":2,
     "estado":true,
     "nombre":"Encuesta IHC 360 semestre 2",
     "descripcion":"Contesten la encuesta",
     "tipo_encuesta_id":1,
     "preguntas":[{"id":1,
    	       "enunciado":"¿Como se llama?",
    	       "created_at":"2016-01-17T05:20:41.085Z",
    	       "updated_at":"2016-01-17T05:20:41.085Z"
    	       },
    	      {"id":3,
    	       "enunciado":"¿Como se llaman?",
    	       "created_at":"2016-01-17T05:20:41.108Z",
    	       "updated_at":"2016-01-17T05:20:41.108Z"
    	      }],
     "created_at":"2016-01-17T05:20:41.025Z",
     "updated_at":"2016-01-17T05:20:41.025Z",
     "profesor_id":null
    }

--- app/models/encuestum.rb
+++ app/models/encuestum.rb
@@ -1,5 +1,6 @@
 class Encuestum < ActiveRecord::Base
-  has_many :encuesta_preguntums
+  has_many :_preguntas, class_name: "EncuestaPreguntum", foreign_key: "encuesta_id"
+  has_many :preguntas, through: :_preguntas, source: :pregunta
   has_many :evaluaciones
   belongs_to :tipo_encuestum
   has_many :encuesta_alumnos
