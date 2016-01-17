b5cf751efcc499780a653a9a55e780696fcdb9b4
Ian Mejias <ian.mejias@usach.cl>
2016-01-16 21:45:09 -0300

    Respuestum: Se arreglan referencias a los "Usuarios"
    Ahora se puede acceder al encuestador y encuestado fácilmente.

--- app/models/respuestum.rb
+++ app/models/respuestum.rb
@@ -1,7 +1,7 @@
 class Respuestum < ActiveRecord::Base
  
   belongs_to :evaluacion
-  belongs_to :usuario
-  belongs_to :usuario
+  belongs_to :encuestador, class_name: "Usuario", foreign_key: "encuestador_id"
+  belongs_to :encuestado, class_name: "Usuario", foreign_key: "encuestado_id"
   has_many :respuestum_preguntums
 end
