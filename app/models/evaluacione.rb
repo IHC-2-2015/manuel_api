2a333fb3abaa4c856dea8137412b558af73c3d04
Ian Mejias <ian.mejias@usach.cl>
2016-01-18 23:32:30 -0300

    Se setea el nombre de la evaluacion por defecto
    El nombre está compuesto por el nombre de la encuesta seguido de un
    guión (-) seguido del nombre de la evaluación con el cual se creó.

--- app/models/evaluacione.rb
+++ app/models/evaluacione.rb
@@ -1,5 +1,9 @@
 class Evaluacione < ActiveRecord::Base
-	has_many :respuestas
-	belongs_to :curso
-	belongs_to :encuesta
+  has_many :respuestas, class_name: 'Respuestum', foreign_key: 'evaluacion_id'
+  belongs_to :curso
+  belongs_to :encuesta, class_name: 'Encuestum', foreign_key: 'encuesta_id'
+
+  validates :nombre, presence: true
+
+  before_create -> {self.nombre = self.encuesta.nombre + ' - ' + self.nombre}
 end
