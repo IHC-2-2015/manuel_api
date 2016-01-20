2b9ce1f1e0acbaf70735e57085529e381c984493
Ian Mejias <ian.mejias@usach.cl>
2016-01-20 10:31:35 -0300

    Se crea callback para crear/borrar funcionalidades al ayudante
    Cada vez que se setea el parámetro 'ayudante' de la tabla
    `curso_alumnos` se compara si éste es `true` o `false`. En caso de ser
    `true`, se crean todas las funcionalidades para ese alumno en la tabla
    `funcionalidad_ayudante_cursos` con valor 'permitido' en `false`. En
    caso contrario, se eliminan estas filas.

--- app/models/curso_alumno.rb
+++ app/models/curso_alumno.rb
@@ -4,24 +4,36 @@ class CursoAlumno < ActiveRecord::Base
   belongs_to :alumno, class_name: 'Alumno'
   has_many :funcionalidades, class_name: 'FuncionalidadAyudanteCurso'
 
-  def crear_funcionalidades
-    begin
-      CursoAlumno.transaction do 
-        func = Funcionalidad.all
+  before_create -> {self.ayudante = false}
+  after_save :setear_funcionalidades
 
-        func.each do |f|
-          self.funcionalidades.create(
-            permitido: false,
-            funcionalidad_id: f.id
-          )
-        end 
+  private
+    def setear_funcionalidades
+      if self.ayudante
+        crear_funcionalidades
+      else
+        eliminar_funcionalidades
       end
-    rescue => e
-      raise ActiveRecord::Rollback
     end
-  end
 
-  def eliminar_funcionalidades
-    self.funcionalidades.destroy_all
-  end
+    def crear_funcionalidades
+      begin
+        CursoAlumno.transaction do 
+          func = Funcionalidad.all
+
+          func.each do |f|
+            self.funcionalidades.create(
+              permitido: false,
+              funcionalidad_id: f.id
+            )
+          end 
+        end
+      rescue => e
+        raise ActiveRecord::Rollback
+      end
+    end
+
+    def eliminar_funcionalidades
+      self.funcionalidades.destroy_all
+    end
 end
